b8ec530caeaa35aae1c3c23aa6cdb8b732c50515
Baptiste Daroussin <bapt@FreeBSD.org>
2009-07-30 18:41:53 +0000

    forgot the .h from last commit

---
+++ cplanet.h
@@ -0,0 +1,49 @@
+#ifndef CPLANET_H
+#define CPLANET_H 1
+
+#include <err.h>
+#include <iconv.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <time.h>
+#include <unistd.h>
+
+/* libmrss */
+#include <mrss.h>
+
+/* clearsilver */
+#include <cs/cs.h>
+#include <util/neo_hdf.h>
+#include <util/neo_misc.h>
+
+#define CPLANET_VERSION "0.1"
+
+#define CP_NAME "CPlanet.Posts.%i.Name=%s"
+#define CP_FEEDNAME "CPlanet.Posts.%i.FeedName=%s"
+#define CP_AUTHOR "CPlanet.Posts.%i.Author=%s"
+#define CP_TITLE "CPlanet.Posts.%i.Title=%s"
+#define CP_LINK "CPlanet.Posts.%i.Link=%s"
+#define CP_DATE "CPlanet.Posts.%i.Date=%s"
+#define CP_FORMATED_DATE  "CPlanet.Posts.%i.FormatedDate=%s"
+#define CP_DESCRIPTION "CPlanet.Posts.%i.Description=%s"
+#define CP_TAG "CPlanet.Posts.%i.Tags.%i.Tag=%s"
+
+/* CPlanet Setters */
+
+#define cp_set_name(hdf_dest, ...) hdf_set_valuef(hdf_dest, CP_NAME, ##__VA_ARGS__)
+#define cp_set_feedname(hdf_dest, ...) hdf_set_valuef(hdf_dest, CP_FEEDNAME, ##__VA_ARGS__)
+#define cp_set_author(hdf_dest, ...) hdf_set_valuef(hdf_dest, CP_AUTHOR, ##__VA_ARGS__)
+#define cp_set_title(hdf_dest, ...) hdf_set_valuef(hdf_dest, CP_TITLE, ##__VA_ARGS__)
+#define cp_set_link(hdf_dest, ...) hdf_set_valuef(hdf_dest, CP_LINK, ##__VA_ARGS__)
+#define cp_set_date(hdf_dest, ...) hdf_set_valuef(hdf_dest, CP_DATE, ##__VA_ARGS__)
+#define cp_set_formated_date(hdf_dest, ...) hdf_set_valuef(hdf_dest, CP_FORMATED_DATE, ##__VA_ARGS__)
+#define cp_set_description(hdf_dest, ...) hdf_set_valuef(hdf_dest, CP_DESCRIPTION, ##__VA_ARGS__)
+#define cp_set_tag(hdf_dest, ...) hdf_set_valuef(hdf_dest, CP_TAG, ##__VA_ARGS__)
+
+/* CPlanet Getters */
+
+#endif
